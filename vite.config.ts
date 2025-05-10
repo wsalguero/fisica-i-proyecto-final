f022ae0e0f574a5621d711cc8c286f87f3922334
williamsalguero <wesito2004@gmail.com>
2025-05-10 13:51:46 -0600

    config de vite cambiado para pruebas de las rutas

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    // útil localmente pero no afecta a producción
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  // 👇 IMPORTANTE para Vercel y SPAs
+  base: "/",
 });
